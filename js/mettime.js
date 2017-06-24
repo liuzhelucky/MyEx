ac94d24182b65057f63c0e6e6e82ad13cb04a550
Netcan <1469709759@qq.com>
2017-06-24 11:21:39 +0800

    The love is over.

--- js/mettime.js
+++ js/mettime.js
@@ -1,7 +1,7 @@
 function mettime() {
 	var MetTime = new Date('2015/10/19 00:00:00');
 	var LoveTime = new Date('2016/02/14 00:00:00');
-	var NowTime = new Date();
+	var NowTime = new Date('2017/06/24 11:00:00');
 	var mt = NowTime.getTime() - MetTime.getTime();
 	var lt = NowTime.getTime() - LoveTime.getTime();
 	var	md=Math.floor(mt/1000/60/60/24);
